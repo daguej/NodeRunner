6636397bebbe5295aade3bb59d2bf6cc0e86d5e0
Josh Dague <daguej@email.uc.edu>
2011-11-17 23:23:01 -0500

    First version.  Includes fancy icon.

---
+++ NodeRunner/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NodeRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var info = System.Diagnostics.Process.GetCurrentProcess();
+            var proc = new System.Diagnostics.ProcessStartInfo(System.Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles) + @"\nodejs\node.exe", "\"" + info.ProcessName + ".js\" " + String.Join(" ", args));
+            proc.UseShellExecute = false;
+            System.Diagnostics.Process.Start(proc);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+Node Runner
+===========
+
+Very simple app to make launching Node scripts easier on Windows.  This app will run Node with a script of the same name as the exe file, passing any arguments along.
+
+Usage
+=====
+
+Assuming we've named the exe `app.exe`:
+
+    C:\folder> app arg1 [...]
+
+Is basically the same as:
+
+    C:\folder> "%PROGRAMFILES%\nodejs\node.exe" "app.js" arg1 [...]
+
+This app does assume Node is installed in a folder named `nodejs` in your system's 32-bit Program Files folder.
